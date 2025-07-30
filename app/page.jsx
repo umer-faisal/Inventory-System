8fbcd2e3c7003d924eb63d6223c76e9af9ea695d
umer-faisal <hafizumerfaisal@gmail.com>
2025-07-30 01:05:56 -0700

    update

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from "react"
 import { useRouter } from "next/navigation"
+import Image from "next/image"
 import { Eye, EyeOff, Lock, User } from "lucide-react"
 
 export default function LoginPage() {
@@ -35,8 +36,8 @@ export default function LoginPage() {
     <div className="min-h-screen gradient-bg flex items-center justify-center p-4">
       <div className="bg-white rounded-2xl shadow-2xl p-8 w-full max-w-md">
         <div className="text-center mb-8">
-          <div className="w-16 h-16 gradient-bg rounded-full flex items-center justify-center mx-auto mb-4">
-            <Lock className="w-8 h-8 text-white" />
+          <div className="w-16 h-16  rounded-full flex items-center justify-center mx-auto mb-4">
+            <Image src="/assests/logo.png" alt="Logo" width={64} height={64} />
           </div>
           <h1 className="text-2xl font-bold text-gray-800">Inventory Management</h1>
           <p className="text-gray-600 mt-2">Sign in to your account</p>
